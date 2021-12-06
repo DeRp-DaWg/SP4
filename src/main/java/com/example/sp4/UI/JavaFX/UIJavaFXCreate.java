e46127028113c4fc4bc44693e1a4e08150e757d3
notebrr <90389727+notebrr@users.noreply.github.com>
2021-12-06 12:20:47 +0100

    Hello

--- src/main/java/com/example/sp4/UI/JavaFX/UIJavaFXCreate.java
+++ src/main/java/com/example/sp4/UI/JavaFX/UIJavaFXCreate.java
@@ -3,8 +3,9 @@ package com.example.sp4.UI.JavaFX;
 import com.example.sp4.UI.UICreate;
 
 public class UIJavaFXCreate implements UICreate {
+
     @Override
-    public void UIShowCreate() {
+    public void UIShowCreate(String surveyType) {
 
     }
 }
