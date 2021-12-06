574f1bbbf463119315d07dd5ab07f52363c3f3b1
Lars Peter Jørgensen <lars-games@outlook.com>
2021-12-06 11:00:28 +0100

    UIShowAnswer() and UIShowCreate()

--- src/main/java/com/example/sp4/UI/UICreate.java
+++ src/main/java/com/example/sp4/UI/UICreate.java
@@ -1,4 +1,7 @@
 package com.example.sp4.UI;
 
 public interface UICreate {
+    //Det her interface skal bruges til at lave svar til et spørgeskema.
+    //UIShowCreate starter dialoget.
+    void UIShowCreate();
 }
