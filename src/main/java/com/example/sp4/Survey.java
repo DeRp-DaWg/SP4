36ddd8b3790e922f46c4147039d6590dbc0b6d81
AndersMeinicke <andersmeinicke@gmail.com>
2021-12-06 11:44:59 +0100

    Hello

--- src/main/java/com/example/sp4/Survey.java
+++ src/main/java/com/example/sp4/Survey.java
@@ -1,9 +1,36 @@
 package com.example.sp4;
 
+import com.example.sp4.Question.MultipleChoice;
 import com.example.sp4.Question.Question;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Survey {
+    public String getName() {
+        return name;
+    }
+
+    String name;
 ArrayList<Question> listOfQuestions;
+
+/*public void addQuestion(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What do you want to ask?");
+    String questionTitle = scanner.nextLine();
+    System.out.println("How many answers do you want?");
+    int amountOfAnswers = scanner.nextInt();
+    Question question = new MultipleChoice(questionTitle,amountOfAnswers);
+    listOfQuestions.add(question);
+}
+//Skeleton to if we add more kinds of questions
+public void addQuestion(String input){
+    switch (input){
+        case "MultipleChoice":
+            Question question = new MultipleChoice();
+            listOfQuestions.add(question);
+            break;
+    }
+}
+*/
 }
