b7765b0ad038d48816078b163d7cc45d1da3209e
notebrr <90389727+notebrr@users.noreply.github.com>
2021-12-07 10:09:58 +0100

    Hello

--- src/main/java/com/example/sp4/IO/IODatabase.java
+++ src/main/java/com/example/sp4/IO/IODatabase.java
@@ -1,7 +1,18 @@
 package com.example.sp4.IO;
 
-public class IODatabase {
+import com.example.sp4.Survey;
+
+public class IODatabase implements IO{
     public void uploadSurvey(){}
     public void getSurvey(){}
 
+    @Override
+    public Survey read(String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void save(Survey survey) {
+
+    }
 }
