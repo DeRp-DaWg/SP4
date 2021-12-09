b6db629a7455790bb28ffe60a050ad1e95f3d871
AndersMeinicke <andersmeinicke@gmail.com>
2021-12-09 12:19:45 +0100

    Testede rundt på HelloApplication

--- src/main/java/com/example/sp4/Main.java
+++ src/main/java/com/example/sp4/Main.java
@@ -1,10 +1,39 @@
 package com.example.sp4;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       Controller controller = new Controller();
+      Controller controller = new Controller();
       //Anders test
-       // HelloApplication helloApplication = new HelloApplication();
-       // helloApplication.hello = "Oh No";
+        ArrayList<String> ohNo = new ArrayList<>();
+        ArrayList<String> ohNo1 = new ArrayList<>();
+        ArrayList<String> ohNo2 = new ArrayList<>();
+        ArrayList<ArrayList<String>> questions = new ArrayList<>();
+        questions.add(ohNo);
+        questions.add(ohNo1);
+        questions.add(ohNo2);
+        String type = "";
+        ohNo.add("Pizza");
+        ohNo.add("Pasta");
+        ohNo.add("Pumpkin");
+        ohNo1.add("Potion");
+        ohNo1.add("Pancake");
+        ohNo2.add("Hello");
+        ohNo2.add("ohNo");
+        ohNo2.add("average");
+        ohNo2.add("goblin");
+
+       /* Scanner scanner = new Scanner(System.in);
+        while(!type.equals("no")){
+            type = scanner.nextLine();
+            if(!type.equals("no")){
+                ohNo.add(type);
+            }
+        }*/
+       HelloApplication helloApplication = new HelloApplication();
+       helloApplication.setQuestions(questions);
+       helloApplication.launchHello();
     }
 }
