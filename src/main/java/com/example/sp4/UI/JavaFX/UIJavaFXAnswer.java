de0eead2f9bad9732d7a5207c15232f68805b4a8
AndersMeinicke <andersmeinicke@gmail.com>
2021-12-13 11:01:26 +0100

    Testede rundt på HelloApplication

--- src/main/java/com/example/sp4/UI/JavaFX/UIJavaFXAnswer.java
+++ src/main/java/com/example/sp4/UI/JavaFX/UIJavaFXAnswer.java
@@ -5,6 +5,7 @@ import com.example.sp4.UI.UIAnswer;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,9 +13,11 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.skin.TextInputControlSkin;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,15 +39,22 @@ public class UIJavaFXAnswer extends UIJavaFX implements Initializable {
         for(int i = 0; i < survey.getQuestions().size();i++){
             ToggleGroup toggleGroup = new ToggleGroup();
             VBox questionBox = new VBox();
+            Insets insets = new Insets(10);
+            questionBox.setPadding(insets);
+            questionBox.setSpacing(20);
             Label questionTitle = new Label();
+            //The string in the new font dosen't change anything, I chose to write something because it looked weird when it was blank
+            Font font = new Font("System", 24);
+            questionTitle.setFont(font);
             Label questionDescription = new Label();
-            questionTitle.setId(survey.getQuestions().get(i).getQuestionName());
+            questionTitle.setText(survey.getQuestions().get(i).getQuestionName());
             questionBox.getChildren().add(questionTitle);
-            questionDescription.setId(survey.getQuestions().get(i).getQuestionDescription());
+            questionDescription.setText(survey.getQuestions().get(i).getQuestionDescription());
             questionBox.getChildren().add(questionDescription);
+            String[] answers = survey.getQuestions().get(i).getAnswers().keySet().toArray(new String[0]);
             for (int j = 0; j < survey.getQuestions().get(i).getAnswers().size(); j++){
                 RadioButton radioButton = new RadioButton();
-                radioButton.setId(survey.getQuestions().get(i).getAnswers().toString());
+                radioButton.setText(answers[j]);
                 radioButton.setToggleGroup(toggleGroup);
                 questionBox.getChildren().add(radioButton);
             }
