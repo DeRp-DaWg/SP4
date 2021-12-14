97a07276952fb6c041aaa145f79deae77b0c21b8
notebrr <90389727+notebrr@users.noreply.github.com>
2021-12-14 15:03:15 +0100

    Big small changes

--- src/main/java/com/example/sp4/UI/JavaFX/UIJavaFXRead.java
+++ src/main/java/com/example/sp4/UI/JavaFX/UIJavaFXRead.java
@@ -3,15 +3,22 @@ package com.example.sp4.UI.JavaFX;
 import com.example.sp4.Question.Question;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -22,27 +29,56 @@ public class UIJavaFXRead extends UIJavaFX implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        int counter;
-        ObservableList<PieChart.Data> pieChart = null;
+        int value = -1;
         for(Question i : survey.getQuestions()){
-            counter = 0;
+            ObservableList<PieChart.Data> pieChart = null;
+            ArrayList<PieChart.Data> pieChartAdd = new ArrayList<>();
             Label questions = new Label(i.getQuestionName());
             Font font = new Font("system", 24);
             questions.setFont(font);
             questions.setPadding(new Insets(0, 0, 10, 0));
             vBox.getChildren().add(questions);
             for(Map.Entry<String, Integer> b : i.getAnswers().entrySet()){
-                Label answers = new Label(b.getKey());
+                String key = b.getKey().toString();
+                value = Integer.parseInt(i.getAnswers().get(b.getKey()).toString());
+                System.out.println("boi" + value);
+
+                Label answers = new Label(value + " people answered " + b.getKey());
                 Font font1 = new Font("system", 14);
                 answers.setFont(font1);
                 answers.setPadding(new Insets(0, 0, 5, 0));
                 vBox.getChildren().add(answers);
-                counter++;
-                pieChart = FXCollections.observableArrayList(new PieChart.Data(i.getQuestionName(), counter));
-                PieChart chart = new PieChart(pieChart);
-                chart.setTitle("Graf");
-                vBox.getChildren().add(chart);
+
+                pieChartAdd.add(new PieChart.Data(b.getKey(), value));
             }
+            pieChart = FXCollections.observableArrayList(pieChartAdd);
+
+            PieChart chart = new PieChart(pieChart);
+            chart.setTitle("Graph");
+            vBox.getChildren().add(chart);
+        }
+
+
+        Button bt = new Button("Back");
+        vBox.getChildren().add(bt);
+
+        bt.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                backButton();
+            }
+        });
+    }
+
+    @FXML
+    protected void backButton(){
+        try {
+            FXMLLoader createFXMLLoader = new FXMLLoader(getClass().getResource("Start.fxml"));
+            Scene scene = new Scene(createFXMLLoader.load(), 600, 400);
+            stage.setScene(scene);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
